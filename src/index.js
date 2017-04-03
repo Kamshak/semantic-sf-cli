b96cec92ae41cfb0352e727958e88eb00920fb32
Valentin Funk <funk.valentin@gmail.com>
2017-04-03 14:44:19 +0200

    fix(cli): Use semantic-sf-release

--- src/index.js
+++ src/index.js
@@ -93,7 +93,7 @@ Aliases:
   pkg.version = '0.0.0-development'
 
   pkg.scripts = pkg.scripts || {}
-  pkg.scripts['sf-release'] = 'semantic-release pre && semantic-release genlog && scriptfodder-publish && semantic-release post'
+  pkg.scripts['sf-release'] = 'semantic-sf-release pre && semantic-sf-release genlog && scriptfodder-publish && semantic-sf-release post'
 
   pkg.repository = pkg.repository || {
     type: 'git',
